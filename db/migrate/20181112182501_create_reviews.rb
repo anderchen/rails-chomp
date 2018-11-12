c2a9080902fdb0ece446c2476bd0f45bcc2f7cd2
Anderson Chen <ander.chen00@gmail.com>
2018-11-12 16:39:13 -0200

    did the migration

--- db/migrate/20181112182501_create_reviews.rb
+++ db/migrate/20181112182501_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.test :content
+      t.text :content
       t.string :score
       t.references :user, foreign_key: true
       t.references :product, foreign_key: true
