c13d58458ce6758e0242b605f7d0c950c863df3c
Stefano Tosi Butori <butori@gmail.com>
2018-11-13 11:37:51 -0200

    Made some preliminary routes based on User Stories

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :products, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
+  resources :diets, only: [:create]
 end
