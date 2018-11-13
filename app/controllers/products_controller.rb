dcf812bd817868e3e34ce394a7d7ab4b352622cb
Stefano Tosi Butori <butori@gmail.com>
2018-11-13 17:40:57 -0200

    Added option for admin user to validate products and did form for new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,41 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show]
+  before_action :set_product, only: [:validate, :show, :edit, :update]
   def index
     @products = Product.all
   end
 
   def show
+  end
+
+  def validate
+    @product.validation = true if current_user.admin?
+    @product.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_users
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
   end
 
   private
@@ -14,6 +45,10 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :photo, :validation, :brand, :category)
+    if current_user.admin?
+      params.require(:product).permit(:name, :description, :validation, :photo, :brand, :category)
+    else
+      params.require(:product).permit(:name, :description, :photo, :brand, :category)
+    end
   end
 end
