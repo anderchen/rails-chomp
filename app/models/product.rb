04cdaffb4f26d44d254a643b87d28767eed750c2
Stefano Tosi Butori <butori@gmail.com>
2018-11-15 16:38:04 -0200

    Refactored code for association between Diet and Product models, using simple form association

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,15 @@ class Product < ApplicationRecord
 
   belongs_to :user
 
-  has_many :product_restrictions, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  has_many :product_restrictions, dependent: :destroy
+  has_many :diets, through: :product_restrictions
 
   validates :name, presence: true
   validates :description, presence: true
   validates :user_id, presence: true
   validates :validation, default: false
 
+  accepts_nested_attributes_for :diets, :product_restrictions
 end
