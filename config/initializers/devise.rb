043021a9793f9105adb87d3df31646acac228abb
Anderson Chen <ander.chen00@gmail.com>
2018-11-15 09:49:10 -0200

    Made some change with profile pic in the user show. Also got facebook profile pic.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # config.omniauth :facebook, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"], scope: 'email',
         info_fields: 'email, first_name, last_name',
-        image_size: 'square',  # 50x50, guaranteed ratio
+        image_size: 'large',  # 50x50, guaranteed ratio
         secure_image_url: true
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
