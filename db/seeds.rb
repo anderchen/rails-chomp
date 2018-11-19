24c70a1cfb09460badc8203f9422fd270299bd70
Anderson Chen <ander.chen00@gmail.com>
2018-11-18 23:40:13 -0200

    Mr veggys product seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,8 +39,8 @@ require 'nokogiri'
 #     product.category = "Ice-cream"
 #     product.user_id = 1
 #     product.name = element.css("h1[itemprop=name]").text.strip
-#     product.ingredients = element.css(".package-ingredients").text
-#     product.traces = element.css(".package-allergy_info").text
+#     product.ingredients = element.css(".package-ingredients").text.strip
+#     product.traces = element.css(".package-allergy_info").text.strip
 #     product.save!
 #   end
 # end
@@ -64,7 +64,6 @@ require 'nokogiri'
 #       product_href << element.attribute('href').value
 #     end
 #   end
-#   puts counter
 #   counter += 1
 # end
 
@@ -81,12 +80,19 @@ require 'nokogiri'
 #     product.brand = "Taeq"
 #     product.user_id = 1
 #     product.name = element.css(".product-details-title").text.strip
-
 #     product.ingredients = element.css(".product-details-ingredients").text.strip
+
+#     product.photo = element.css(".product-details-image img").attr("src")
 
 #     # Using regex to filter the allergens and put it in the traces column
 #     traces = element.css(".product-details-ingredients").text.strip
-#     product.traces = traces[/contém.\D*/i]
+#     # Checks if it contains gluten or not in the ingredients
+#     if traces =~ /não contém.\D*/i
+#       product.traces = traces[/não contém.\D*/i]
+#     else
+#       product.traces = traces[/contém.\D*/i]
+#     end
+
 #     product.save!
 #   end
 # end
@@ -94,7 +100,52 @@ require 'nokogiri'
 # puts "Taeq successfully added!"
 
 
+# Scraping products from Mr Veggy
+puts "Getting Mr Veggy's products..."
 
+# Getting Mr Veggy's category href
+category_href = []
 
+url_index = "http://mrveggy.com/produtos-mr-veggy/"
+html_file_index = open(url_index).read
+html_doc_index = Nokogiri::HTML(html_file_index)
 
+html_doc_index.search(".img-produto a").each do | element|
+  category_href << element.attribute('href').value
+end
 
+# Getting Mr Veggy's product href
+product_href = []
+category_href.each do |category|
+
+  url_index = category
+  html_file_index = open(url_index).read
+  html_doc_index = Nokogiri::HTML(html_file_index)
+
+  html_doc_index.search(".data a").each do | element|
+    product_href << element.attribute('href').value
+  end
+end
+new_product_href, odd = product_href.partition.with_index { |_,i| i.even? }
+
+# Getting information of each Mr Veggy's product
+new_product_href.each do |product|
+  puts "Going in #{product}..."
+
+  url_index = product
+  html_file_index = open(url_index).read
+  html_doc_index = Nokogiri::HTML(html_file_index)
+
+  html_doc_index.search(".produto-classicos").each do | element|
+    product = Product.new
+    product.brand = "Mr. Veggy"
+    product.user_id = 1
+    product.name = element.css(".data h1").text.strip
+    product.ingredients = element.css(".full-content p").first.text.strip
+    product.traces = element.css("strong").first.text.strip
+    product.photo = element.css(".img img").attr("src")
+    product.save!
+  end
+end
+
+puts "Mr Veggy's products successfully add"
