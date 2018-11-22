4ba57328feb1a3502fe47a3fc119bfd58e1474af
Anderson Chen <ander.chen00@gmail.com>
2018-11-22 12:48:55 -0200

    Search bar at navbar fixed

--- app/javascript/components/button.js
+++ app/javascript/components/button.js
@@ -2,8 +2,7 @@ function expandSearchBarOnClick(){
   const searchField = document.querySelector(".search-box-collapse");
 
   const addSearchBar = (event) => {
-    event.preventDefault();
-    console.log("Here!");
+    // event.preventDefault();
     searchField.classList.toggle("search-box-expand");
     searchField.classList.toggle("search-box-collapse");
     searchField.focus();
